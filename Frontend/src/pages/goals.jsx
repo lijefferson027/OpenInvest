36ef9d77405f43c1c100df3849cfcb48345e10bb
Peter Nguyen <peterhnguyen@Peters-Air.lan>
2024-03-22 13:55:49 -0400

    pushing changes

--- Frontend/src/pages/goals.jsx
+++ Frontend/src/pages/goals.jsx
@@ -2,11 +2,13 @@ import React from "react";
 //For Each page import the Sidebar Component in order to navigate to different pages
 import MiniDrawer from "../assets/components/sidebar";
 import { Box } from "@mui/material";
+import WipeccButtonComponent from "../assets/components/buttons/wipecc";
 const goals = () => {
         return (<>
         <Box sx={{ display: 'flex' }} >
         <MiniDrawer />
-        <div className="App">GOALS PAGE ROUTE</div>
+        <WipeccButtonComponent />
+        <div className="App"> </div>
         </Box>
     </>) 
 };
