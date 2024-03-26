140c1fd83a441b9cc5ba661527bf6e6bb230cb94
Christina Nguyen <christina.nguyen8711@gmail.com>
2024-03-26 00:37:45 -0400

    Added stripe server

--- Frontend/src/pages/payments.jsx
+++ Frontend/src/pages/payments.jsx
@@ -1,9 +1,12 @@
-//Subpages to Accounts page
-import React from "react";
+//Subpages to Accounts page | add stripe to payments page
+import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 //For Each page import the Sidebar Component in order to navigate to different pages
 import MiniDrawer from "../assets/components/sidebar";
 import { Box } from "@mui/material";
 import SimpleBottomNavigation from "../assets/components/accnavbar";
+
 const payments = () => {
     return (<>
     <Box sx={{ display: 'flex' }} >
