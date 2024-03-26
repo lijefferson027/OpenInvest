b55b66ec4f6f77332b55da70e21a76a029924f17
Nteaman <126529267+Nteaman@users.noreply.github.com>
2024-03-25 22:07:50 -0400

    Update1

---
+++ Frontend/my-login-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginPage from './LoginPage';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/my-login-app/src/LoginPage.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+import './index.css';
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    // to  login send a request to a backend API and validate existing 
+    //  check if the username and password match "admin"
+    if (username === 'openInvest' && password === 'openInvest') {
+      setLoggedIn(true);
+    } else {
+      alert('Invalid username or password');
+    }
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+    setUsername('');
+    setPassword('');
+  };
+
+
+  return (
+    <div>
+      {loggedIn ? (
+        <div>
+          <p>Welcome back, {username}!</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default LoginPage;
