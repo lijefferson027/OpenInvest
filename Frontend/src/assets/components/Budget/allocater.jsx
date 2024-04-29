c8ddeb09411b88c4dfae25d87246f88e2472c289
Peter Nguyen <kwnpeter@gmail.com>
2024-04-29 01:03:19 -0400

    Budget Page Implemented Allocater Component

---
+++ Frontend/src/assets/components/Budget/allocater.jsx
@@ -0,0 +1,296 @@
+import React, { useState } from "react";
+import {
+  Button,
+  MenuItem,
+  Menu,
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
+  Typography,
+} from "@mui/material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+function Allocater() {
+  const initialCategories = [
+    "Stocks",
+    "Crypto",
+    "ETFs",
+    "Index Funds",
+    "Bonds",
+    "Mutual Funds",
+    "Roth IRA",
+    "401k",
+    "Travel",
+    "Miscellaneous",
+  ];
+  const [budget, setBudget] = useState("");
+  const [categories, setCategories] = useState(initialCategories);
+  const [allocations, setAllocations] = useState(
+    initialCategories.reduce((acc, category) => ({ ...acc, [category]: 0 }), {})
+  );
+  const [priorities, setPriority] = useState(
+    initialCategories.reduce((acc, category) => ({ ...acc, [category]: 1 }), {})
+  );
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const prioritiesList = ["Omit", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const handleBudgetChange = (e) => {
+    setBudget(e.target.value);
+  };
+
+  const handlePriorityChange = (category, value) => {
+    if (value === "Omit") {
+      setPriority({ ...priorities, [category]: 0 });
+    } else {
+      setPriority({ ...priorities, [category]: parseInt(value) });
+    }
+    handleClose();
+  };
+
+  const handleClick = (event, category) => {
+    setAnchorEl(event.currentTarget);
+    setSelectedCategory(category);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const distributeFunds = (method) => {
+    if (!budget || isNaN(budget) || parseFloat(budget) <= 0) {
+      setAllocations(
+        categories.reduce((acc, category) => ({ ...acc, [category]: 0 }), {})
+      );
+      return;
+    }
+
+    const totalPriority = Object.values(priorities).reduce(
+      (sum, num) => sum + num,
+      0
+    );
+
+    switch (method) {
+      case "even":
+        const evenAllocation = parseFloat(budget) / categories.length;
+        setAllocations(
+          categories.reduce(
+            (acc, category) => ({ ...acc, [category]: evenAllocation }),
+            {}
+          )
+        );
+        break;
+      case "random":
+        let totalAllocated = 0;
+        const randomAllocations = categories.map((category, index) => {
+          const remaining = parseFloat(budget) - totalAllocated;
+          const allocation =
+            index < categories.length - 1
+              ? Math.random() * remaining
+              : remaining;
+          totalAllocated += allocation;
+          return { category, allocation };
+        });
+        setAllocations(
+          randomAllocations.reduce(
+            (acc, { category, allocation }) => ({
+              ...acc,
+              [category]: allocation,
+            }),
+            {}
+          )
+        );
+        break;
+      case "priority":
+        if (totalPriority === 0) return;
+        setAllocations(
+          categories.reduce((acc, category) => {
+            const allocatedAmount =
+              (priorities[category] / totalPriority) * parseFloat(budget);
+            return { ...acc, [category]: allocatedAmount };
+          }, {})
+        );
+        break;
+      default:
+        break;
+    }
+  };
+
+  const resetDefaults = () => {
+    setCategories(initialCategories);
+    setPriority(
+      initialCategories.reduce(
+        (acc, category) => ({ ...acc, [category]: 1 }),
+        {}
+      )
+    );
+    setBudget("");
+    setAllocations(
+      initialCategories.reduce(
+        (acc, category) => ({ ...acc, [category]: 0 }),
+        {}
+      )
+    );
+  };
+
+  return (
+    <div >
+      <input
+        type="number"
+        value={budget}
+        onChange={handleBudgetChange}
+        placeholder="Enter your budget"
+        style={{
+          width: "30vw",
+          height: "5vh",
+          backgroundColor: "transparent",
+          color: "black",
+          borderRadius: 5,
+        }}
+      />
+      <div>
+        {/* Buttons to distribute funds */}
+        <Button
+          onClick={() => distributeFunds("even")}
+          style={{
+            backgroundColor: "#8DDAB8",
+            border: "solid",
+            borderColor: "#37be83",
+            color: "white",
+            fontWeight: "bold",
+            padding: "5px 22px",
+            textAlign: "center",
+            display: "inline-block",
+            fontSize: "16px",
+            margin: "4px 2px",
+            cursor: "pointer",
+            borderRadius: "5px",
+          }}
+        >
+          Distribute Evenly
+        </Button>
+        <Button
+          onClick={() => distributeFunds("random")}
+          style={{
+            backgroundColor: "#8DDAB8",
+            border: "solid",
+            borderColor: "#37be83",
+            color: "white",
+            fontWeight: "bold",
+            padding: "5px 22px",
+            textAlign: "center",
+            display: "inline-block",
+            fontSize: "16px",
+            margin: "4px 2px",
+            cursor: "pointer",
+            borderRadius: "5px",
+          }}
+        >
+          Distribute Randomly
+        </Button>
+        <Button
+          onClick={() => distributeFunds("priority")}
+          style={{
+            backgroundColor: "#8DDAB8",
+            border: "solid",
+            borderColor: "#37be83",
+            color: "white",
+            fontWeight: "bold",
+            padding: "5px 22px",
+            textAlign: "center",
+            display: "inline-block",
+            fontSize: "16px",
+            margin: "4px 2px",
+            cursor: "pointer",
+            borderRadius: "5px",
+          }}
+        >
+          Distribute by Priority
+        </Button>
+      </div>
+      <div >
+        <List>
+          {categories.map((category, index) => (
+            <React.Fragment key={category}>
+              <ListItem sx={{ fontWeight: "bold" }}>
+                <Button
+                  aria-controls={anchorEl ? "priority-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={anchorEl ? "true" : undefined}
+                  onClick={(e) => handleClick(e, category)}
+                >
+                  <KeyboardArrowDownIcon />
+                  Priority: {priorities[category] || "Set"}
+                </Button>
+                <ListItemText
+                  primary={
+                    <Typography
+                      variant="body1"
+                      style={{
+                        fontFamily: "sans-serif",
+                        color: "#565254",
+                        fontWeight: "bold",
+                        fontSize: "18px",
+                        marginLeft: "1rem",
+                      }}
+                    >
+                      {category}
+                    </Typography>
+                  }
+                />
+                <div style={{ marginLeft: "1rem" }}>
+                  Allocation: $
+                  {allocations[category]
+                    ? allocations[category].toFixed(2)
+                    : "0.00"}
+                </div>
+              </ListItem>
+              {index < categories.length - 1 && <Divider />}
+            </React.Fragment>
+          ))}
+        </List>
+      </div>
+      <Menu
+        id="priority-menu"
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        MenuListProps={{ "aria-labelledby": "priority-button" }}
+      >
+        {prioritiesList.map((option) => (
+          <MenuItem
+            key={option}
+            onClick={() => handlePriorityChange(selectedCategory, option)}
+            disableRipple
+          >
+            Priority {option}
+          </MenuItem>
+        ))}
+      </Menu>
+      <Button
+        onClick={resetDefaults}
+        style={{
+          backgroundColor: "#e84855",
+          border: "solid",
+          borderColor: "#565254",
+          color: "white",
+          fontWeight: "bold",
+          padding: "5px 22px",
+          textAlign: "center",
+          display: "inline-block",
+          fontSize: "16px",
+          margin: "4px 2px",
+          cursor: "pointer",
+          borderRadius: "5px",
+          left: "20em",
+        }}
+      >
+        Reset
+      </Button>
+    </div>
+  );
+}
+
+export default Allocater;
--- Frontend/src/pages/budget.jsx
+++ Frontend/src/pages/budget.jsx
@@ -1,66 +1,14 @@
-import React, { useState } from 'react';
-import MiniDrawer from '../assets/components/sidebar';
-
-
-function budget() {
-  const [totalBudget, setTotalBudget] = useState(0);
-  const [bucketValues, setBucketValues] = useState({
-    "Stocks": 0,
-    "Crypto": 0,
-    "ETFs": 0,
-    "Index Funds": 0,
-    "Bonds": 0,
-    "Mutual Funds": 0,
-    "Roth IRA": 0,
-    "401 k": 0,
-    "Travel": 0,
-    "Miscellaneous": 0
-  });
-
-  const updateBucketValues = () => {
-    const total = parseFloat(totalBudget);
-    const numBuckets = 10;
-    const equalAmount = (total * 1) / numBuckets;
-    const updatedBuckets = {};
-    for (const key in bucketValues) {
-      updatedBuckets[key] = equalAmount.toFixed(1);
-    }
-    setBucketValues(updatedBuckets);
-  };
-
-  const handleTotalBudgetChange = (e) => {
-    const { value } = e.target;
-    setTotalBudget(value);
-    updateBucketValues();
-  };
-
-  return (
-    <box>
-      <MiniDrawer/>
-    <form>
-      <label>
-        Total Budget:
-        <input
-          type="number"
-          value={totalBudget}
-          onChange={handleTotalBudgetChange}
-        />
-      </label>
-      {Object.keys(bucketValues).map((category) => (
-        <div key={category}>
-          <label>
-            {category}:
-            <input
-              type="text"
-              readOnly
-              value={bucketValues[category]}
-            />
-          </label>
-        </div>
-      ))}
-    </form>
-    </box>
-  );
-}
-
+import React from "react";
+//For Each page import the Sidebar Component in order to navigate to different pages
+import MiniDrawer from "../assets/components/sidebar";
+import { Box } from "@mui/material";
+import Allocater from "../assets/components/Budget/allocater";
+const budget = () => {
+        return (<>
+        <Box >
+        <MiniDrawer />
+        <Allocater/>
+        </Box>
+    </>) 
+};
 export default budget;
